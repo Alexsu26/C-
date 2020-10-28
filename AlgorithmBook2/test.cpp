c40eb5649df6a03add255e3e7188d1cfd8cfb403
Xu_no_ghost <l18181138587@163.com>
2020-10-28 18:49:56 +0800

    暂时完成4-1,使用udebug网站首个数据通过，次个不完全通过，但无法通过uva，使用他人数据能通过uva但udebug次个数据同样不完全相同 --laptop

--- AlgorithmBook2/test.cpp
+++ AlgorithmBook2/test.cpp
@@ -1,43 +1,11 @@
-#include <cstdio>
+#include <iostream>
 using namespace std;
-#define maxn 25
-int n,k,m,a[maxn];
-
-int go(int p, int d, int t);
-
 int main(int argc, char const *argv[])
 {
-    while (scanf("%d%d%d",&n,&k,&m) == 3 && n)
-    {
-        for(int i=1; i<=n; i++)
-            a[i] = i;
-        int left = n;
-        int p1 = n,p2 = 1;//
-        while (left)
-        {
-            p1 = go(p1,1,k);
-            p2 = go(p2,-1,m);
-            printf("%3d",p1);
-            left--;
-            if(p2 != p1)
-            {
-                printf("%3d",p2);
-                left--;
-            }
-            a[p1] = a[p2] = 0;
-            if(left)
-                printf(",");
-        }
-        printf("\n");
-    }
+    int x,y;
+    char name;
+    
+    cout<<x<<" "<<y<<" "<<name<<endl;
     return 0;
 }
 
-int go(int p,int d, int t){
-    while(t--){
-        do{
-            p = (p+d+n-1) % n + 1;
-        }while(a[p] == 0);
-    }
-    return p;
-}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,2 @@
-ILE *src;
-    // src = fopen(".\files.txt","w");
-    // int a = 100;
-    // char b[23] = "hello world!";
-    // fprintf(src,"a = %d,b = %s\n",a,b);
-    // fclose(src);
-    // system(
+if( k==5 && RGC == -1)
+                    //     flag = 0;
