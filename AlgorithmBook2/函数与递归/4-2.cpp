3c7dc3ed25161de281db9bc1f425630780683a2e
Xu_no_ghost <l18181138587@163.com>
2020-10-29 23:06:55 +0800

    通过4-2 --laptop

--- AlgorithmBook2/函数与递归/4-2.cpp
+++ AlgorithmBook2/函数与递归/4-2.cpp
@@ -1,25 +1,32 @@
+// 输入包含了多个游戏棋盘。
+// 每个棋盘包含了 n ^ 2 个点的正方形矩阵(其中 2 ≤ n ≤ 9) ，
+// 以及一些起连接作用的横向或纵向的线段。
+// 棋盘的的 n ^ 2 个点和 m 条连接线段，格式如下：
+#define LOCAL
 #include <stdio.h>
 #include <string.h>
 
 int check(int x, int y, int size);
 
-int H[9][9], V[9][9];
+int H[10][10], V[10][10];
 
 int main(int argc, char const *argv[])
 {
+#ifdef LOCAL
     freopen("test.in", "r", stdin);
     freopen("test.out", "w", stdout);
+#endif
     int n;
     int count = 0;
     while (scanf("%d", &n) == 1)
     {
-        if( count )
+        if (count)
             printf("\n**********************************\n\n");
         int num;
         scanf("%d", &num);
         memset(H, 0, sizeof(H));
         memset(V, 0, sizeof(V));
-        int cnt[9];
+        int cnt[10];
         memset(cnt, 0, sizeof(cnt));
         int flag = 0;
         while (num--)
@@ -45,39 +52,39 @@ int main(int argc, char const *argv[])
                     }
                 }
             }
-            printf("Problem #%d\n\n", ++count);
-            if(flag)
+        printf("Problem #%d\n\n", ++count);
+        if (flag)
+        {
+            for (int i = 1; i <= n; i++)
             {
-                for (int i = 1; i <= n; i++)
-                {
-                    if (cnt[i] > 0)
-                        printf("%d square (s) of size %d\n", cnt[i], i);
-                }
-                // printf("\n");
+                if (cnt[i] > 0)
+                    printf("%d square (s) of size %d\n", cnt[i], i);
             }
-            else
-                printf("No completed squares can be found.\n");
+            // printf("\n");
+        }
+        else
+            printf("No completed squares can be found.\n");
     }
     return 0;
 }
 int check(int x, int y, int size)
 {
-    int flagx = 0,flagy = 0,i;
-    for(i=y;i<y+size;i++)
+    int flagx = 0, flagy = 0, i;
+    for (i = y; i < y + size; i++)
     {
-        if( H[x][i] == 0 || H[x+size][i] == 0 )
+        if (H[x][i] == 0 || H[x + size][i] == 0)
             break;
     }
-    if(i == (y+size) )
+    if (i == (y + size))
         flagx = 1;
     for (i = x; i < x + size; i++)
     {
-        if ( V[i][y] == 0 || V[i][y+size] == 0)
+        if (V[i][y] == 0 || V[i][y + size] == 0)
             break;
     }
     if (i == (x + size))
         flagy = 1;
-    if( flagx && flagy)
+    if (flagx && flagy)
         return 1;
     else
         return 0;
--- test.cpp
+++ test.cpp
@@ -1,236 +1,87 @@
+#define LOCAL
 #include <stdio.h>
 #include <string.h>
 
-struct xiangqi
-{
-    char name;
-    int x;
-    int y;
-} info[8];
+int check(int x, int y, int size);
 
-int qipan[11][10];
-int RGC = 0, H = 0, i;
+int H[10][10], V[10][10];
 
-int checkinfo(int x, int y)
-{
-    for (i = 0; i < 10; i++)
-    {
-        if (info[i].x == x && info[i].y == y)
-            return i;
-    }
-    return -1;
-}
-int isB(int x, int y)
-{
-    int k = checkinfo(x, y);
-    if (info[k].name == 'B')
-        return 1;
-    else
-        return 0;
-}
-int checkRGC(int x, int y, char name, int k)
-{
-    int count = 0;
-    int inLine = 0;
-    if (info[0].x == x)
-    {
-        inLine = 1;
-        int add = info[0].y > y ? 1 : -1;
-        for (i = y + add; i != info[0].y; i += add)
-            if (qipan[x][i])
-                count++;
-    }
-    else if (info[0].y == y)
-    {
-        inLine = 1;
-        int add = info[0].x > x ? 1 : -1;
-        for (i = x + add; i != info[0].x; i += add)
-            if (qipan[i][y])
-                count++;
-    }
-    if ((name == 'R') && inLine)
-    {
-        if (count)
-            RGC = 0;
-        else
-            RGC = 1;
-    }
-    else if ((name == 'G') && inLine)
-    {
-        if (k == 5)
-        {
-            RGC = -1;
-        }
-        else
-        {
-            if (count)
-                RGC = 0;
-            else
-                RGC = 1;
-        }
-    }
-    else if (name == 'C' && inLine)
-    {
-        if (count == 1)
-            RGC = 1;
-        else
-            RGC = 0;
-    }
-    else
-        RGC = 0;
-    return 0;
-}
-int checkH(int x, int y)
-{
-    if (isB(x + 2, y + 1) && (qipan[x + 1][y] != 1))
-        H = 1;
-    else if (isB(x + 2, y - 1) && (qipan[x + 1][y] != 1))
-        H = 1;
-    else if (isB(x - 2, y + 1) && (qipan[x - 1][y] != 1))
-        H = 1;
-    else if (isB(x - 2, y - 1) && (qipan[x - 1][y] != 1))
-        H = 1;
-    else if (isB(x + 1, y + 2) && (qipan[x][y + 1] != 1))
-        H = 1;
-    else if (isB(x - 1, y + 2) && (qipan[x][y + 1] != 1))
-        H = 1;
-    else if (isB(x + 1, y - 2) && (qipan[x][y - 1] != 1))
-        H = 1;
-    else if (isB(x - 1, y - 2) && (qipan[x][y - 1] != 1))
-        H = 1;
-    else
-        H = 0;
-    return 0;
-}
-int legal(int x, int y)
-{
-    if (y < 4 || y > 6 || x < 1 || x > 3)
-        return 0;
-    else
-        return 1;
-}
-int clear(int num)
-{
-    qipan[info[num].x][info[num].y] = 0;
-    info[num].name = 'A';
-    info[num].x = info[num].y = 0;
-    return 0;
-}
-int add(int x, int y)
-{
-    info[0].name = 'B';
-    info[0].x = x;
-    info[0].y = y;
-    qipan[x][y] = 1;
-    return 0;
-}
-void print(int n)
-{
-    for (int i = 0; i < n; i++)
-        printf("%c %d %d\n", info[i].name, info[i].x, info[i].y);
-    for (int i = 1; i < 11; i++)
-    {
-        for (int j = 1; j < 10; j++)
-            printf("%d ", qipan[i][j]);
-        printf("\n");
-    }
-    return;
-}
 int main(int argc, char const *argv[])
 {
+#ifdef LOCAL
     freopen("test.in", "r", stdin);
     freopen("test.out", "w", stdout);
-    memset(qipan, 0, sizeof(qipan));
+#endif
     int n;
-    int move[4][2] = {0, -1, -1, 0, 0, 1, 1, 0};
-    int x, y;
-    while (scanf("%d%d%d", &n, &x, &y) == 3 && n)
+    int count = 0;
+    while (scanf("%d", &n) == 1)
     {
-        int flag;
-        int cnt = 1;
-        while (n--)
+        if (count)
+            printf("\n**********************************\n\n");
+        int num;
+        scanf("%d", &num);
+        memset(H, 0, sizeof(H));
+        memset(V, 0, sizeof(V));
+        int cnt[10];
+        memset(cnt, 0, sizeof(cnt));
+        int flag = 0;
+        while (num--)
         {
+            char x;
+            int h, v;
             getchar();
-            scanf("%c%d%d", &info[cnt].name, &info[cnt].x, &info[cnt].y);
-            qipan[info[cnt].x][info[cnt].y] = 1;
-            cnt++;
+            scanf("%c%d%d", &x, &h, &v);
+            if (x == 'H')
+                H[h][v] = 1;
+            else
+                V[v][h] = 1;
         }
-        //算出下一步
-        for (int k = 0; k < 6; k++)
-        {
-            int x1, y1;
-            flag = 1, RGC = H = 0;
-            // add(x,y);
-            if (k < 4)
+        for (int i = 1; i < n; i++)
+            for (int j = 1; j < n; j++)
             {
-                x1 = x + move[k][0];
-                y1 = y + move[k][1];
-            }
-            if (k == 4)
-            {
-                x1 = 2;
-                y1 = 5;
-            }
-            if (k == 5)
-            {
-                x1 = x;
-                y1 = y;
-            }
-            if (legal(x1, y1))
-            {
-                int num;
-                // clear(0);
-                int is = checkinfo(x1, y1);
-                if (is != -1)
-                    clear(is);
-                add(x1, y1);
-                // print(cnt);
-                for (num = 1; num < cnt; num++)
+                for (int k = 1; k <= n; k++)
                 {
-                    switch (info[num].name)
+                    if (check(i, j, k))
                     {
-                    case 'R':
-                        checkRGC(info[num].x, info[num].y, 'R', k);
-                        break;
-                    case 'H':
-                        checkH(info[num].x, info[num].y);
-                        break;
-                    case 'C':
-                        checkRGC(info[num].x, info[num].y, 'C', k);
-                        break;
-                    case 'G':
-                        checkRGC(info[num].x, info[num].y, 'G', k);
-                        break;
+                        cnt[k]++;
+                        flag = 1;
                     }
-                    if ((RGC == 1) || (H == 1))
-                        break;
-                    // if( k==5 && RGC == -1)
-                    //     flag = 0;
-                }
-                if (num == cnt)
-                {
-                    if (k == 5)
-                    {
-                        if (RGC == -1)
-                            flag = 0;
-                        else
-                            flag = 1;
-                    }
-                    else
-                        flag = 0;
                 }
             }
-            clear(0);
-            if (!flag)
-            {
-                printf("NO\n");
-                break;
-            }
-        }
-
+        printf("Problem #%d\n\n", ++count);
         if (flag)
-            printf("YES\n");
-        memset(qipan, 0, sizeof(qipan));
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (cnt[i] > 0)
+                    printf("%d square (s) of size %d\n", cnt[i], i);
+            }
+            // printf("\n");
+        }
+        else
+            printf("No completed squares can be found.\n");
     }
     return 0;
+}
+int check(int x, int y, int size)
+{
+    int flagx = 0, flagy = 0, i;
+    for (i = y; i < y + size; i++)
+    {
+        if (H[x][i] == 0 || H[x + size][i] == 0)
+            break;
+    }
+    if (i == (y + size))
+        flagx = 1;
+    for (i = x; i < x + size; i++)
+    {
+        if (V[i][y] == 0 || V[i][y + size] == 0)
+            break;
+    }
+    if (i == (x + size))
+        flagy = 1;
+    if (flagx && flagy)
+        return 1;
+    else
+        return 0;
 }
