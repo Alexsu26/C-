1c8b873ec1152145153604c0282b6fd215849d35
Xu_no_ghost <l18181138587@163.com>
2020-12-21 21:36:14 +0800

    更新5-1    --laptop

--- AlgorithmBook2/C++与STL入门/例题5-1_uva10474.cpp
+++ AlgorithmBook2/C++与STL入门/例题5-1_uva10474.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    
-
+    const int maxn = 10000;
+    int n, q, x, a[maxn], flag = 0;
+    while (cin >> n >> q && n)
+    {
+        cout << "CASE# " << ++flag << ":\n";
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        sort(a, a + n);
+        while (q--)
+        {
+            cin >> x;
+            int p = lower_bound(a, a + n, x) - a;
+            if (a[p] == x)
+                cout << x << " found at " << p + 1 << endl;
+            else
+                cout << x << " not found\n";
+        }
+    }
     return 0;
 }
