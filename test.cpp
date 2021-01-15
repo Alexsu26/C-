168d186e6a6bd7847e392fd369bb337d9e3b02fb
Xu_no_ghost <l18181138587@163.com>
2021-01-15 17:46:51 +0800

    完成例题5-2，vector部分

--- test.cpp
+++ test.cpp
@@ -1,26 +1,18 @@
-#include <algorithm>
-#include <iostream>
+#include <bits.\stdc++.h>
 using namespace std;
 
 int main()
 {
-    const int maxn = 10000;
-    int n,q,x,a[maxn],flag=0;
-    while(cin>>n>>q && n)
-    {
-        cout << "CASE# " << ++flag <<":\n";
-        for(int i=0; i<n; i++)
-            cin >> a[i];
-        sort(a,a+n);
-        while(q--)
-        {
-            cin >> x;
-            int p = lower_bound(a,a+n,x) - a ;
-            if( a[p] == x )
-                cout<< x <<" found at " << p+1 <<endl;
-            else
-                cout<< x << " not found\n";
-        }
-    }
+    vector<int>test;
+    int n = 10;
+    for(int i=0; i<n; i++)
+        test.push_back(i);
+    
+    test.pop_back();
+
+    for(int i=0; i<test.size(); i++)
+        cout<< test[i] << "\n";
+    cout<< "test.size() = " << test.size() <<
+    "\ntest(9) = " << test[9] << endl;
     return 0;
 }
