f1279747651c2b2ee2241384837d6e8aa0598cac
Xu_no_ghost <l18181138587@163.com>
2021-01-20 11:57:17 +0800

    完成例题5-7    --laptop

--- AlgorithmBook2/C++与STL入门/例题5-7_uva136.cpp
+++ AlgorithmBook2/C++与STL入门/例题5-7_uva136.cpp
@@ -4,30 +4,29 @@
 // #include <vector>
 using namespace std;
 
-
 typedef long long LL;
-const int n[3] = {2,3,5};
+const int n[3] = {2, 3, 5};
 int main()
 {
-    priority_queue<LL,vector<LL>,greater<LL> >pq;
-    set<LL>num;
+    priority_queue<LL, vector<LL>, greater<LL>> pq;
+    set<LL> num;
     pq.push(1);
     num.insert(1);
 
-    for(int i=1; ; i++)
+    for (int i = 1;; i++)
     {
-        if(i == 1500)
+        if (i == 1500)
         {
             cout << "The 1500'th ugly number is "
-            << pq.top() << ".\n"; 
+                 << pq.top() << ".\n";
             break;
         }
         LL x = pq.top();
         pq.pop();
-        for(int j=0; j<3; j++)
+        for (int j = 0; j < 3; j++)
         {
             LL x1 = x * n[j];
-            if( !num.count(x1))
+            if (!num.count(x1))
             {
                 num.insert(x1);
                 pq.push(x1);
