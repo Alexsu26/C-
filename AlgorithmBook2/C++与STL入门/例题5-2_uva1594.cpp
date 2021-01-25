4bb1a80c9e7b521f8ac131d676b521c1d05f4f61
xunoghost <xu_no_ghost@163.com>
2021-01-25 18:25:25 +0800

    完成习题5-2    --laptop

--- AlgorithmBook2/C++与STL入门/例题5-2_uva1594.cpp
+++ AlgorithmBook2/C++与STL入门/例题5-2_uva1594.cpp
@@ -2,11 +2,11 @@
 #include <algorithm>
 using namespace std;
 
-void loop(int *A,int len)
+void loop(int *A, int len)
 {
     int a0 = A[0];
     int i;
-    for(i=0; i<len - 1; i++)
+    for (i = 0; i < len - 1; i++)
         A[i] = abs(A[i] - A[i + 1]);
     A[i] = abs(A[i] - a0);
 }
@@ -17,27 +17,27 @@ int main()
     // freopen("test.out","w",stdout);
     const int maxn = 15 + 5;
     int Ducci[maxn];
-    int n,num;
+    int n, num;
     cin >> n;
-    while(n--)
+    while (n--)
     {
         cin >> num;
-        for(int i=0; i < num; i++)
+        for (int i = 0; i < num; i++)
             cin >> Ducci[i];
-        
-        for(int i=0; i<1005; i++)
-            loop(Ducci,num);
-        
+
+        for (int i = 0; i < 1005; i++)
+            loop(Ducci, num);
+
         bool flag = true;
-        for(int i=0; i<num; i++)
+        for (int i = 0; i < num; i++)
         {
-            if(Ducci[i] != 0)
+            if (Ducci[i] != 0)
             {
                 flag = false;
                 break;
             }
         }
-        if(flag)
+        if (flag)
             cout << "ZERO\n";
         else
             cout << "LOOP\n";
