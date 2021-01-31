94a077bd6a9f6df8ea0f942af4596cebb5e4a194
Leo <l18181138587@163.com>
2021-01-31 19:37:29 +0800

    更新    ----laptop

--- test.cpp
+++ test.cpp
@@ -1,10 +1,13 @@
-#include <bits./stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 const int maxn = 10000;
 int dots[2*maxn][2*maxn];
 int main()
 {
+    freopen("test.in","r",stdin);
+    freopen("test.out","w",stdout); 
     int n;
     cin >> n;
     while(n--)
@@ -25,23 +28,52 @@ int main()
             miny = min(miny,y);
             maxy = max(maxy,y);
         }
-        for(int i=minx-1; i<maxx+1; i++)
+        minx += maxn;
+        maxx += maxn;
+        miny += maxn;
+        maxy += maxn;
+        vector<double>symm;
+        bool flag = true;
+        for(int i=minx; i<=maxx; i++)
         {
             vector<int>lines;
-            for(int j=miny-1; j<maxy+1; j++)
+            for(int j=miny; j<=maxy; j++)
             {
                 if(dots[i][j] == 1)
                     lines.push_back(dots[i][j]);
             }
             if(lines.size() % 2 == 0)
             {
-
+                double sum = 0;
+                for(int i=0; i<lines.size(); i++)
+                    sum += lines[i];
+                symm.push_back(sum/2.0);
             }
             else
             {
-                
+                int i;
+                for(i=0; i<lines.size()/2; i++)
+                {
+                    if(lines[i] != lines[lines.size() - i])
+                    {
+                        flag = false;
+                        break;
+                    }
+                    symm.push_back(lines[i]);
+                }
             }
         }
+        for(int j=0; j<symm.size() - 1; j++)
+            if(symm[j] != symm[j+1])
+            {
+                flag = false;
+                break;
+            }
+        
+        if(flag)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
     }
     return 0;
 }
