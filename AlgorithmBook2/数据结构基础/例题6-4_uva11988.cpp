cf49bd76fe2a1558c516151621821702d329601b
xunoghost <xu_no_ghost@163.com>
2021-02-07 23:02:10 +0800

    更新例题6-4    ----laptop

--- AlgorithmBook2/数据结构基础/例题6-4_uva11988.cpp
+++ AlgorithmBook2/数据结构基础/例题6-4_uva11988.cpp
@@ -7,34 +7,34 @@ int next[maxn];
 char s[maxn];
 int main()
 {
-    freopen("test.in","r",stdin);
-    freopen("test.out","w",stdout);
+    freopen("test.in", "r", stdin);
+    freopen("test.out", "w", stdout);
     // char s[maxn];
-    while(scanf("%s",s+1) == 1)
+    while (scanf("%s", s + 1) == 1)
     {
-        int len = strlen(s+1);
-        int cur=0,last=0;
+        int len = strlen(s + 1);
+        int cur = 0, last = 0;
         // int next[maxn];
         next[0] = 0;
-        for(int i=1; i<=len; i++)
+        for (int i = 1; i <= len; i++)
         {
             char c = s[i];
-            if(c == '[')
+            if (c == '[')
                 cur = 0;
-            else if(c == ']')
+            else if (c == ']')
                 cur = last;
             else
             {
                 next[i] = next[cur];
                 next[cur] = i;
-                if(cur == last)
+                if (cur == last)
                     last = i;
                 cur = i;
             }
         }
 
-        for(int i = next[0]; i != 0; i = next[i])
-            printf("%c",s[i]);
+        for (int i = next[0]; i != 0; i = next[i])
+            printf("%c", s[i]);
         printf("\n");
     }
     return 0;
