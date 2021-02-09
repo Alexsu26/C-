99738d56e279923003fdc61a17cfe6bc724922f6
xunoghost <xu_no_ghost@163.com>
2021-02-09 12:40:27 +0800

    完成例题6-5，有很多细节可以参考     ----laptop

--- AlgorithmBook2/数据结构基础/例题6-5_uva12657.cpp
+++ AlgorithmBook2/数据结构基础/例题6-5_uva12657.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 const int maxn = 100000 + 10;
-int _left_[maxn],_right_[maxn];
+int _left_[maxn], _right_[maxn];
 
 void link(int l, int r)
 {
@@ -13,38 +13,38 @@ void link(int l, int r)
 
 int main()
 {
-    freopen("test.in","r",stdin);
-    freopen("test.out","w",stdout);
-    int n,m;
+    freopen("test.in", "r", stdin);
+    freopen("test.out", "w", stdout);
+    int n, m;
     int kase = 0;
-    while(scanf("%d%d",&n,&m) == 2)
+    while (scanf("%d%d", &n, &m) == 2)
     {
-        for(int i=1; i<=n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            _left_[i] = i-1;
-            _right_[i] = (i+1) % (n+1);
+            _left_[i] = i - 1;
+            _right_[i] = (i + 1) % (n + 1);
         }
         _left_[0] = n;
         _right_[0] = 1;
 
         int op;
         bool inv = false;
-        int X,Y;
-        while(m--)
+        int X, Y;
+        while (m--)
         {
-            scanf("%d",&op);
-            if(op == 4)
+            scanf("%d", &op);
+            if (op == 4)
                 inv = !inv;
             else
             {
                 scanf("%d%d", &X, &Y);
                 if (inv && op != 3)
                     op = 3 - op;
-                if(op == 3 && _right_[Y] == X)
-                    swap(X,Y);//
-                if(op == 1 && X == _left_[Y])
+                if (op == 3 && _right_[Y] == X)
+                    swap(X, Y);//简化后面的代码，可以将x在y两侧的情况简化为只在一侧的情况
+                if (op == 1 && X == _left_[Y])
                     continue;
-                if(op == 2 && X == _right_[Y])
+                if (op == 2 && X == _right_[Y])
                     continue;
 
                 int LX = _left_[X], RX = _right_[X];
@@ -76,21 +76,21 @@ int main()
                         link(Y, X);
                         link(X, RY);
                     }
-                }     
-            }     
+                }
+            }
         }
 
         int x = 0;
-        long long ans = 0;
-        for(int i=1; i<=n; i++)
+        long long ans = 0;//n可达100000，int型不足以存储
+        for (int i = 1; i <= n; i++)
         {
             x = _right_[x];
-            if(i % 2 == 1)
+            if (i % 2 == 1)
                 ans += x;
         }
-        if(inv && n % 2 == 0)
-            ans = (long long)n*(n+1)/2 - ans;
-        printf("Case %d: %lld\n",++kase, ans);
+        if (inv && n % 2 == 0)//n为奇偶会影响正反时奇数的编号
+            ans = (long long)n * (n + 1) / 2 - ans;
+        printf("Case %d: %lld\n", ++kase, ans);//longlong型输出用%lld
     }
     return 0;
 }
