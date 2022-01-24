93b2b08d0ee3e3e125e381af3d978bd4be7fcfee
Xunoghost <xu_no_ghost@163.com>
2022-01-24 22:59:17 +0800

    递归    ----laptop

--- 算法竞赛进阶指南/0x00基本算法/递归/AcWing92.cpp
+++ 算法竞赛进阶指南/0x00基本算法/递归/AcWing92.cpp
@@ -7,27 +7,30 @@ int n;
 
 vector<int> chosed;
 
-void calc(int x){
-    if( x == n+1 ){
-        for( int i=0; i<chosed.size(); i++ ){
+void calc(int x)
+{
+    if (x == n + 1)
+    {
+        for (int i = 0; i < chosed.size(); i++)
+        {
             printf("%d ", chosed[i]);
         }
         printf("\n");
         return;
     }
     //not chosed
-    calc(x+1);
+    calc(x + 1);
 
     //chosed
     chosed.push_back(x);
-    calc(x+1);
+    calc(x + 1);
 
     chosed.pop_back();
 }
 
 int main(int argc, char const *argv[])
 {
-    scanf("%d",&n);
+    scanf("%d", &n);
     calc(1);
     return 0;
 }
